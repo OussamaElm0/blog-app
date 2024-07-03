1d62b41d1d766c7b634094cab88bc2d3edad8498
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-07-03 22:38:33 +0100

    Fetch posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+const env = import.meta.env;
 
-function App() {
-  const [count, setCount] = useState(0)
+const PostsContext = createContext();
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const { data } = await axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/posts`);
+        setPosts(data);
+      })();
+      console.log(posts);
+    } catch (e) {
+      console.log(e.message);
+    }
+    console.log(env.VITE_REACT_APP_API_URL);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World</h1>
     </>
-  )
+  );
 }
-
-export default App
