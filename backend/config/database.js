5c3afd3d2d8c34471d2fd68665a9465a30a968c9
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-07-08 13:03:05 +0100

    Add localhost db in connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,11 +3,16 @@ require("dotenv").config();
 
 // Temporary measure for testing the connection
 const db_connection =
-  process.env.DB_CONNECTION ;
+  process.env.DB_CONNECTION || "mongodb://localhost:27017/blog-app" ;
 
 const connectDatabase = () => {
   mongoose
-    .connect(db_connection)
+    .connect(db_connection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
     .then(() => console.log("MongoDB connected with server"))
     .catch((e) => console.log(e.message));
 };
