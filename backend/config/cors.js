5434aa12778ff6d5ac8f295938d6eec604e11f79
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-07-09 13:02:56 +0100

    Update cors origins

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,13 +1,6 @@
-const allowedOrigins = ["http://localhost:3000", "https://blog-oe.vercel.app"]; // Add your port number for localhost if it's not 3000
-
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (allowedOrigins.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true, credentials: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
+const corsOptions = {
+  origin: ["blog-app-pi-rosy.vercel.app/", "http://localhost:5173/"],
+  credentials: true,
 };
 
-module.exports = corsOptionsDelegate;
+module.exports = corsOptions;
