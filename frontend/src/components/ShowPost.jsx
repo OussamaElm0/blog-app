6f43c0659ebb6929e8966a5d2345ce3e3b232c09
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-07-11 11:22:34 +0100

    Show post

--- frontend/src/components/ShowPost.jsx
+++ frontend/src/components/ShowPost.jsx
@@ -1,20 +1,62 @@
-import React ,{ useContext, useEffect, useState } from 'react'
-import PostsContext from '../contexts/PostContext'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import PostsContext from "../contexts/PostContext";
+import { useParams, Link } from "react-router-dom";
+import styles from "../styles/posts.module.scss";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import Post from "./Post"
 
 export default function ShowPost() {
-    const [post, setPost] = useState({})
-    const posts = useContext(PostsContext)
-    const { post_id } = useParams()
+  const [post, setPost] = useState(null);
+  const [otherPosts, setOtherPosts] = useState([])
+  const [loading, setLoading] = useState(true);
+  const posts = useContext(PostsContext);
+  const { post_id } = useParams();
 
-    useEffect(() => {
-        const post = posts.find((post) => post._id == post_id);
-        setPost(post)
-    }, [])
+  useEffect(() => {
+    const post = posts.find((post) => post._id === post_id);
+    const other_posts = posts.filter((post) => post._id !== post_id);
+    other_posts.slice(5);
+    if (post !== undefined) {
+      setPost(post);
+      setOtherPosts(other_posts)
+      setLoading(false);
+    } else {
+      setLoading(true);
+    }
+  }, [posts, post_id]);
 
+  if (loading || !post) {
     return (
-        <>
-            {post.content}
-        </>
-    )
+      <span className={styles.loading}>
+        <PacmanLoader />
+      </span>
+    );
+  }
+
+  return (
+    <>
+      <div className={styles.show_post_container}>
+        <div className={styles.post}>
+          <h1 className={`${styles.content}`}>{post.content}</h1>
+          <div className={styles.tags}>
+            {post.tags.map((tag, index) => {
+              return (
+                <span key={index} className={`${styles.tag} kanit-4`}>
+                  {tag}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.other_posts}>
+            {otherPosts.map(post => {
+                return <Post key={post._id} {...post} />
+            })}
+            <Link to="/" className={styles.see_more}>
+                See more...
+            </Link>
+        </div>
+      </div>
+    </>
+  );
 }
